1ba411b1bf6e0086729bb84611f741fae734bb74
Suraj Pheudin <pheudinsuraj@gmail.com>
2023-03-25 20:51:55 +0545

    feat: completly update chakra-ui

--- templates/react/chakra-ui/src/pages/index.tsx
+++ templates/react/chakra-ui/src/pages/index.tsx
@@ -1,4 +1,8 @@
+import {Button} from "@chakra-ui/react";
 const Home = ()=> {
-    return <div>Home</div>
+    return (<div>Home
+        <Button>Solid Primary</Button>
+        <Button colorScheme="secondary">Solid Secondary</Button>
+        </div>)
 }
 export default Home
